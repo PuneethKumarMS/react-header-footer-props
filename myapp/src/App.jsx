02f275f588a6395cff7c8b962a7e76c501350461
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-01-08 15:06:10 +0530

    Update App.jsx

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./components/Header";
+import Header from "./components/Header"; /* folder/files */
 import Footer from "./components/Footer";
-import './App.css';
+import './App.css'; /*styles*/
 
 function App() {
   return(
