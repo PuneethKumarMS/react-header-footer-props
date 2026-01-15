10aa2894461a016225f91a5088bf5763a30db8b9
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-01-15 07:34:09 +0530

    Update Footer.jsx

--- myapp/src/components/Footer.jsx
+++ myapp/src/components/Footer.jsx
@@ -2,15 +2,10 @@ import React from "react";
 
 function Footer(props) {
     return(
-
     <footer className="footer">
-
         <p>{props.tagline}</p>
         <p>{props.copyright}</p>
-
     </footer>
     );
-
-
 }
 export default Footer;
